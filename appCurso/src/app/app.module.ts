98b2e8aad59ce5f20907731567c901101cd0c7fc
Jorge Luis Ortiz Castillo <georgelois03@gmail.com>
2017-11-28 23:06:39 -0500

    Seccion02-11: Componente ejmetodo
    Creacion y edicion del componente ejmetodo

--- appCurso/src/app/app.module.ts
+++ appCurso/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { FechaActualComponent } from './fechaactual/fechaactual.component';
 import { CopyrightComponent } from './copyright/copyright.component';
 import { ViewmodeloComponent } from './viewmodelo/viewmodelo.component';
+import { EjmetodoComponent } from './ejmetodo/ejmetodo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FechaActualComponent,
     CopyrightComponent,
-    ViewmodeloComponent
+    ViewmodeloComponent,
+    EjmetodoComponent
   ],
   imports: [
     BrowserModule,

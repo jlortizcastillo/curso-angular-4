9a8335c980838f64e27416faff85a05397ff29bb
Jorge Luis Ortiz Castillo <georgelois03@gmail.com>
2017-12-07 14:37:25 -0500

    Seccion03-25: @Input
    Creacion de componentes padre e hijo

---
+++ appCurso/src/app/padre/padre.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-padre',
+  templateUrl: './padre.component.html',
+  styleUrls: ['./padre.component.css']
+})
+export class PadreComponent implements OnInit {
+
+  /*valorPadre: string = 'Este texto se inyecta desde el Padre';*/
+  avisos: string[] = ['Aviso 1', 'Aviso 2', 'Aviso 3'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

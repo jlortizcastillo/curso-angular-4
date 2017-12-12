cd1e9a001a98dbdc98c94385dabb27c77f6fedb6
Jorge Luis Ortiz Castillo <georgelois03@gmail.com>
2017-12-11 21:52:42 -0500

    Seccion04-27: Pipe Date
    Edicion del componente FechaactualComponent

--- appCurso/src/app/fechaactual/fechaactual.component.ts
+++ appCurso/src/app/fechaactual/fechaactual.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-fechaactual',
     template: `
-        <p>Lima, {{ hoy | date: 'd/M/y H:m' }}</p>
+        <p>Lima, {{ hoy | date: 'd-M-y' }} a las {{ hoy | date: 'H:m Z'}}</p>
         <app-copyright></app-copyright>
     `,
     styleUrls: ['./fechaactual.component.css']

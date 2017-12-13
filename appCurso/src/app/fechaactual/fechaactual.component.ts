1381d075de36425a4aa0d2c4332b642ed616aa27
Jorge Luis Ortiz Castillo <georgelois03@gmail.com>
2017-12-13 00:11:53 -0500

    Seccion04-30: Pipe currency
    Edicion del componente FechaActualComponent

--- appCurso/src/app/fechaactual/fechaactual.component.ts
+++ appCurso/src/app/fechaactual/fechaactual.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-fechaactual',
     template: `
-        <p>El resultado es {{ resultado | number: '3.2-2' }}</p>
+        <p>La cotizacion actual del dolar es de {{ dolareuro | currency: 'EUR': true }}</p>
     `,
     styleUrls: ['./fechaactual.component.css']
 })
 export class FechaActualComponent {
-    resultado: number = 1.2;
+    dolareuro: number = 0.88;
 }

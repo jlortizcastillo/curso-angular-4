401a779476a0645afa281c57507af1b09abbd434
Jorge Luis Ortiz Castillo <georgelois03@gmail.com>
2017-12-13 17:02:51 -0500

    Seccion04-32: Pipe propio
    Edicion del componente FechaActualComponent

---
+++ appCurso/src/app/euro.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'euro'
+})
+export class EuroPipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    const euro = value + ' €';
+
+    return euro;
+  }
+
+}
--- appCurso/src/app/fechaactual/fechaactual.component.ts
+++ appCurso/src/app/fechaactual/fechaactual.component.ts
@@ -3,12 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-fechaactual',
     template: `
-        <p>{{ sexo | i18nSelect: encabezamiento}} {{ nombre }}</p>
+        <p>La cotizacion actual del dolar es de {{ dolareuro | euro }}</p>
     `,
     styleUrls: ['./fechaactual.component.css']
 })
 export class FechaActualComponent {
-    nombre: string = 'Jorge';
-    sexo: string = 'hombre';
-    encabezamiento: any =  {'hombre': 'Estimado', 'mujer': 'Estimada'};
+    dolareuro: number = 0.88;
 }

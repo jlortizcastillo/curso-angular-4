39535a5517100e16572f66a8f102f8da5fdd8309
Jorge Luis Ortiz Castillo <georgelois03@gmail.com>
2017-12-13 00:01:16 -0500

    Seccion04-28: Pipes uppercase y lowercase
    Edicion del componente FechaactualComponent

--- appCurso/src/app/fechaactual/fechaactual.component.ts
+++ appCurso/src/app/fechaactual/fechaactual.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-fechaactual',
     template: `
-        <p>Lima, {{ hoy | date: 'd-M-y' }} a las {{ hoy | date: 'H:m Z'}}</p>
+        <p> {{ ciudad | lowercase }}, {{ hoy | date: 'd-M-y' }} a las {{ hoy | date: 'H:m Z'}}</p>
         <app-copyright></app-copyright>
     `,
     styleUrls: ['./fechaactual.component.css']
 })
 export class FechaActualComponent {
     hoy = new Date();
+    ciudad: string = 'Lima';
 }

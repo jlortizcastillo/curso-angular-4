b2c5325add7802cc7fd0420a97dbb21b1d766e05
Jorge Luis Ortiz Castillo <georgelois03@gmail.com>
2018-01-06 22:20:36 -0500

    Seccion10-65: Autenticando
    Edicion del componente InisesComponent

--- appCompras/src/app/autenticacion/inises/inises.component.ts
+++ appCompras/src/app/autenticacion/inises/inises.component.ts
@@ -13,6 +13,7 @@ export class InisesComponent implements OnInit {
   loginForm: FormGroup;
   userData: any;
   mensaje = false;
+  autenticando = false;
 
   constructor(private formBuilder: FormBuilder,
               private autenticacionService: AutenticacionService,
@@ -31,11 +32,13 @@ export class InisesComponent implements OnInit {
   }
 
   onSubmit() {
+    this.autenticando = true;
     this.userData = this.saveUserData();
     this.autenticacionService.inicioSesion(this.userData);
     setTimeout(() => {
       if (this.isAuth() === false) {
         this.mensaje = true;
+        this.autenticando = false;
       }
     }, 2000);
   }

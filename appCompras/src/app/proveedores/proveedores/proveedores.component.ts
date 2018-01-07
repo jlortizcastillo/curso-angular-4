cb35f4a232f4aeff54b7c7db99c8d0ace68e7981
Jorge Luis Ortiz Castillo <georgelois03@gmail.com>
2018-01-06 22:13:06 -0500

    Seccion10-64: Spinners
    Edicion del componente ProveedoresComponent

--- appCompras/src/app/proveedores/proveedores/proveedores.component.ts
+++ appCompras/src/app/proveedores/proveedores/proveedores.component.ts
@@ -8,6 +8,7 @@ import { ProveedoresService } from '../../servicios/proveedores.service';
 })
 export class ProveedoresComponent implements OnInit {
   proveedores: any[] = [];
+  cargando = true;
 
   constructor(private proveedoresService: ProveedoresService) {
     this.proveedoresService.getProveedores()
@@ -17,6 +18,8 @@ export class ProveedoresComponent implements OnInit {
                                 p.id$ = id$;
                                 this.proveedores.push(proveedores[id$]);
                               }
+
+                              this.cargando = false;
                             });
   }
 
